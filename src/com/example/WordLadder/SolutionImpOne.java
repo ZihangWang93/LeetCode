06e361c758d5e25518538203a11d05bb131f921b
zihangwang <520chriswang@gmail.com>
2019-07-25 23:55:19 +0800

    Improve time cost by removing the isVisited

--- src/com/example/WordLadder/SolutionImpOne.java
+++ src/com/example/WordLadder/SolutionImpOne.java
@@ -1,4 +1,41 @@
 package com.example.WordLadder;
 
+import java.util.*;
+
 public class SolutionImpOne {
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        HashSet<String> wordSet = new HashSet<>(wordList);
+        HashSet<String> beginSet = new HashSet<>();
+        HashSet<String> endSet = new HashSet<>();
+
+        if (!wordSet.contains(endWord)) return 0;
+        int length = 1;
+        beginSet.add(beginWord);
+        endSet.add(endWord);
+
+        while(!beginSet.isEmpty()) {
+            if (beginSet.size() > endSet.size()) {
+                HashSet<String> temp = beginSet;
+                beginSet = endSet;
+                endSet = temp;
+            }
+
+            HashSet<String> newSet = new HashSet<String>();
+            for(String st : beginSet) {
+                for (int i = 0; i < st.length(); i++) {
+                    for (char c = 'a'; c <= 'z'; c++) {
+                        String newSt = st.substring(0, i) + c + st.substring(i + 1);
+                        if (endSet.contains(newSt)) return length + 1;
+                        if (wordSet.contains(newSt)) {
+                            newSet.add(newSt);
+                            wordSet.remove(newSt);
+                        }
+                    }
+                }
+            }
+            beginSet = newSet;
+            length += 1;
+        }
+        return 0;
+    }
 }
