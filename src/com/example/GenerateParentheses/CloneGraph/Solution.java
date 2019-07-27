7f8277e2e25da0e6235e481d9c67c9cd1f2c0de3
zihangwang <520chriswang@gmail.com>
2019-07-27 17:19:23 +0800

    implement with BFS

---
+++ src/com/example/GenerateParentheses/CloneGraph/Solution.java
@@ -0,0 +1,44 @@
+package com.example.Permutation.CloneGraph;
+
+import java.util.*;
+
+public class Solution {
+
+    class Node {
+        public int val;
+        public List<Node> neighbors;
+
+        public Node() {}
+
+        public Node(int _val,List<Node> _neighbors) {
+            val = _val;
+            neighbors = _neighbors;
+        }
+    }
+
+    public Node cloneGraph(Node node) {
+
+        if (node.neighbors == null || node.neighbors.size() == 0) {
+            return new Node(node.val, new LinkedList<Node>());
+        }
+
+        Node[] clone = new Node[101];
+        Queue<Node> next = new LinkedList<>();
+        clone[node.val] = new Node(node.val, new LinkedList<Node>());
+
+        next.add(node);
+
+        while (!next.isEmpty()) {
+            Node temp = next.remove();
+            for (Node n : temp.neighbors) {
+                if (clone[n.val] == null) {
+                    clone[n.val] = new Node(n.val, new LinkedList<Node>());
+                    next.add(n);
+                }
+                clone[temp.val].neighbors.add(clone[n.val]);
+            }
+        }
+
+        return clone[node.val];
+    }
+}
