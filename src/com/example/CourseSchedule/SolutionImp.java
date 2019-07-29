d25f6426a003abd556a8af72ba1aa064f0623909
zihangwang <520chriswang@gmail.com>
2019-07-29 12:46:49 +0800

    improve performance by replace HashMap with ArrayList[]

--- src/com/example/CourseSchedule/SolutionImp.java
+++ src/com/example/CourseSchedule/SolutionImp.java
@@ -1,4 +1,53 @@
 package com.example.CourseSchedule;
 
+import java.util.*;
+
 public class SolutionImp {
+    private boolean[] isVisited;
+    private boolean[] noLoop;
+
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        if (numCourses == 1 || prerequisites == null || prerequisites.length == 0) {
+            return true;
+        }
+
+        ArrayList[] edges = new ArrayList[numCourses];
+
+        for (int i = 0; i < numCourses; i++) {
+            edges[i] = new ArrayList<Integer>();
+        }
+
+        for (int i = 0; i < prerequisites.length; i++) {
+            edges[prerequisites[i][0]].add(prerequisites[i][1]);
+        }
+
+        isVisited = new boolean[numCourses];
+        noLoop = new boolean[numCourses];
+
+        for (int i = 0; i < numCourses; i++) {
+            if (!dfs(edges, i)) return false;
+        }
+
+        return true;
+    }
+
+    private boolean dfs(ArrayList<Integer>[] edges, int index) {
+
+        if (isVisited[index]) { return false;}
+
+        if (noLoop[index] || edges[index].size() == 0) {
+            return true;
+        }
+
+        isVisited[index] = true;
+        for (int i : edges[index]) {
+            if (!dfs(edges, i)) {
+                return false;
+            }
+        }
+
+        isVisited[index] = false;
+        noLoop[index] = true;
+        return true;
+    }
 }
