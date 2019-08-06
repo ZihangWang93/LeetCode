46ef27d1c333ef6cca4ada5eb4f50f9f1be3cacf
zihangwang <520chriswang@gmail.com>
2019-08-06 11:31:53 +0800

    improve the algorithm wiht int[] structure supporting different states

--- src/com/example/IsGraphBipartite/SolutionTwo.java
+++ src/com/example/IsGraphBipartite/SolutionTwo.java
@@ -1,4 +1,30 @@
 package com.example.IsGraphBipartite;
 
-public class SolutionTwo {
+import java.util.*;
+
+class SolutionTwo {
+    public boolean isBipartite(int[][] graph) {
+        int[] color = new int[graph.length];
+
+        Queue<Integer> next = new LinkedList<Integer>();
+        for (int i = 0; i < graph.length; i++) {
+            if (color[i] == 0) {
+                next.offer(i);
+                color[i] = 1;
+                while (!next.isEmpty()) {
+
+                    int cur = next.poll();
+
+                    for (int adj : graph[cur]) {
+                        if (color[adj] == color[cur]) return false;
+                        else if (color[adj] == 0) {
+                            color[adj] = -1 * color[cur];
+                            next.offer(adj);
+                        }
+                    }
+                }
+            }
+        }
+        return true;
+    }
 }
