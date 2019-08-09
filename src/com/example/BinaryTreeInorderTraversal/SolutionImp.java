faa91a76fc7a7982f3bd3c05b593719345e04c39
zihangwang <520chriswang@gmail.com>
2019-08-09 10:29:32 +0800

    improve the code by more concise and readable code

--- src/com/example/BinaryTreeInorderTraversal/SolutionImp.java
+++ src/com/example/BinaryTreeInorderTraversal/SolutionImp.java
@@ -1,4 +1,29 @@
 package com.example.BinaryTreeInorderTraversal;
 
-public class SolutionImp {
+import java.util.*;
+
+class SolutionImp {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+
+    private List<Integer> result;
+    public List<Integer> inorderTraversal(TreeNode root) {
+        result = new LinkedList<>();
+
+        if (root == null) return result;
+        dfs(root);
+        return result;
+    }
+
+    private void dfs(TreeNode node) {
+        if (node != null) {
+            dfs(node.left);
+            result.add(node.val);
+            dfs(node.right);
+        }
+    }
 }
