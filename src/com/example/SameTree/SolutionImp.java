2d186b4e257a8f9fa0d02d025668e2bc9f784274
zihangwang <520chriswang@gmail.com>
2019-08-09 11:31:10 +0800

    improve by making the code much more concise

--- src/com/example/SameTree/SolutionImp.java
+++ src/com/example/SameTree/SolutionImp.java
@@ -1,4 +1,27 @@
 package com.example.SameTree;
 
 public class SolutionImp {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+
+        if (p == null || q == null) {
+            return false;
+        }
+
+        if (p.val != q.val) {
+            return false;
+        }
+
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
 }
