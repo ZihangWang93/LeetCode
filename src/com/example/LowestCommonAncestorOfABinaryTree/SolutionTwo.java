5df670314f34d7329e0b70f0e0f08488ac180d4b
zihangwang <520chriswang@gmail.com>
2019-08-16 18:57:33 +0800

    implement by DFS method

--- src/com/example/LowestCommonAncestorOfABinaryTree/SolutionTwo.java
+++ src/com/example/LowestCommonAncestorOfABinaryTree/SolutionTwo.java
@@ -1,4 +1,20 @@
 package com.example.LowestCommonAncestorOfABinaryTree;
-
+import java.util.*;
 public class SolutionTwo {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || root == p || root == q) return root;
+
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if (left != null && right != null) return root;
+        return left == null ? right : left;
+    }
 }
