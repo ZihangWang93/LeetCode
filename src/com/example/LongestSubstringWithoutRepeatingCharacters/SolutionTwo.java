ddc6904a16161b93bbb78c0b8cf4d8eeaa1b63dc
zihangwang <520chriswang@gmail.com>
2019-08-30 11:58:03 -0400

    implement by smart method

--- src/com/example/LongestSubstringWithoutRepeatingCharacters/SolutionTwo.java
+++ src/com/example/LongestSubstringWithoutRepeatingCharacters/SolutionTwo.java
@@ -1,4 +1,15 @@
 package com.example.LongestSubstringWithoutRepeatingCharacters;
 
 public class SolutionTwo {
+    public int lengthOfLongestSubstring(String s) {
+        int ans = 0;
+        int[] index = new int[128];
+
+        for (int i = 0, j = 0; j < s.length(); j++) {
+            i = Math.max(index[s.charAt(j)], i);
+            ans = Math.max(ans, j - i + 1);
+            index[s.charAt(j)] = j + 1;
+        }
+        return ans;
+    }
 }
