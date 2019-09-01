6e8ea641f622e1802b80e307835c4ef360c4ba49
zihangwang <520chriswang@gmail.com>
2019-08-31 22:00:24 -0400

    implement by recursive method

--- src/com/example/CopyListWithRandomPointer/SolutionTwo.java
+++ src/com/example/CopyListWithRandomPointer/SolutionTwo.java
@@ -1,4 +1,32 @@
 package com.example.CopyListWithRandomPointer;
 
+import java.util.*;
+
 public class SolutionTwo {
+    HashMap<Node, Node> map = new HashMap<>();
+
+    class Node {
+        public int val;
+        public Node next;
+        public Node random;
+
+        public Node() {}
+
+        public Node(int _val,Node _next,Node _random) {
+            val = _val;
+            next = _next;
+            random = _random;
+        }
+    }
+
+    public Node copyRandomList(Node head) {
+        if (head == null) return null;
+
+        if (map.containsKey(head)) return map.get(head);
+        Node node = new Node(head.val, null, null);
+        map.put(head, node);
+        node.next = copyRandomList(head.next);
+        node.random = copyRandomList(head.random);
+        return node;
+    }
 }
