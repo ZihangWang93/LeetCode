2debc719cda68a1b83e6ffd8de8d3a4359c28e87
zihangwang <520chriswang@gmail.com>
2019-09-09 12:36:47 -0400

    implement by Two Pointers

---
+++ src/com/example/TwoSumTwo/SolutioTwo.java
@@ -0,0 +1,16 @@
+package com.example.TwoSumTwo;
+
+public class SolutioTwo {
+    public int[] twoSum(int[] numbers, int target) {
+        int lo = 0, hi = numbers.length - 1;
+
+        while (lo <= hi) {
+            int sum = numbers[lo] + numbers[hi];
+
+            if (sum == target) return new int[]{lo + 1, hi + 1};
+            if (sum > target) hi--;
+            else lo++;
+        }
+        return new int[]{};
+    }
+}
